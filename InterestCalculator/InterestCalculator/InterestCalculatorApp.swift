a098a9d818c15bbf59e73d0ee08832560e431822
Sahil Satralkar <sahil.satralkar@gmail.com>
2024-08-17 14:33:20 +0530

    Initial commit

---
+++ InterestCalculator/InterestCalculator/InterestCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterestCalculatorApp.swift
+//  InterestCalculator
+//
+//  Created by Sahil Satralkar on 17/08/24.
+//
+
+import SwiftUI
+
+@main
+struct InterestCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
