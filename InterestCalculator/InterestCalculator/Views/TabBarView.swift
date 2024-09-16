6b4ac68f98dc81e52b1693d8aedf4d210ba99083
Sahil Satralkar <sahil.satralkar@gmail.com>
2024-09-16 11:08:36 +0530

    Added App Store review prompt

--- InterestCalculator/InterestCalculator/Views/TabBarView.swift
+++ InterestCalculator/InterestCalculator/Views/TabBarView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct TabBarView: View {
+    
+    @Environment(\.requestReview) private var requestReview
+    
     var body: some View {
         TabView {
             CompoundInterestView()
@@ -27,8 +30,29 @@ struct TabBarView: View {
                     Label("Settings", systemImage: "gear")
                 }
         }
+        .onAppear {
+            presentReview()
+        }
+    }
+    
+    private func presentReview() {
+        Task {
+            // Delay for ten seconds to avoid interrupting the person using the app.
+            try await Task.sleep(for: .seconds(10))
+            await requestReview()
+        }
     }
 }
+
+extension Bundle {
+    var releaseVersionNumber: String? {
+        return infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+    var buildVersionNumber: String? {
+        return infoDictionary?["CFBundleVersion"] as? String
+    }
+}
+
 
 
 #Preview {
